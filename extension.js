053d28369089e91f2fd0024b0df14a9273e62c56
Paul Lunow <paul@Pauls-MacBook-Pro.local>
2017-09-20 13:30:31 +0200

    make extension list configurable

--- extension.js
+++ extension.js
@@ -3,13 +3,12 @@
 const vscode = require('vscode');
 const path = require('path');
 
+//gets the file name and an extension list and returns the index of the next extension to test
 const getFileExtension = (fn, exts) => {
     for(let i = 0; i < exts.length; i++) {
         const ext = exts[i];
         const l = (ext.length + 1) * -1
-        // console.log(i, fn, fn.substr(l), ext, fn.substr(l) === ext);
         if(fn.substr(l+1) === ext) {
-            // console.log('found!');
             return i;
         }
     }
@@ -17,17 +16,20 @@ const getFileExtension = (fn, exts) => {
 };
 exports.getFileExtension = getFileExtension;
 
+//return the extension as string instead of the index of extension list
 const getFileExtensionAsString = (fn, exts) => {
     const index = getFileExtension(fn, exts);
     return exts[index] || false;
 };
 exports.getFileExtensionAsString = getFileExtensionAsString;
 
+//wrapper for vscode.workpace.findFiles for mocking it in tests
 const findFiles = (fn) => {
     return vscode.workspace.findFiles(fn);
 };
 exports.findFiles = findFiles;
 
+//this function loops over the extension list and find the files. takes a few parameters for testing it (see tests)
 const findCorrespondingFile = ({ absoluteFilename, allExtensions, rootPath, findFiles }) => {
     return new Promise((resolve, reject) => {
         const absoluteFilePath = path.dirname(absoluteFilename);
@@ -35,26 +37,20 @@ const findCorrespondingFile = ({ absoluteFilename, allExtensions, rootPath, find
         const relativePath = path.relative(rootPath, absoluteFilePath);
         
         //cancel everything, if current file is not in list of all extensions
-        console.log(absoluteFilename, allExtensions);
-        console.log('active file extension:', activeFileExtension);
         if(!activeFileExtension) {
             console.log('file is not in corresponding extension list');
             return;
         }
+        
         const activeFileBasename = path.basename(absoluteFilename, activeFileExtension);
-
         const tryToOpen = (basename, extensions) => {
-            console.log('*** tryToOpen', basename, extensions.join('; '), '***');
             
             const next = () => {
-                console.log('goto next file');
                 extensions.splice(index, 1);
                 if(extensions.length > 0) {
-                    console.log('try to open next file. remaining extensions:', extensions.join('; '));
                     tryToOpen(basename, extensions);
                 }
                 else {
-                    console.log('absolutly no matching file found.');
                     reject('no files found');
                 }
             };
@@ -62,27 +58,22 @@ const findCorrespondingFile = ({ absoluteFilename, allExtensions, rootPath, find
             let index = (getFileExtension(basename, extensions)+1) % extensions.length;
             const extension = extensions[index];
             const currentExt = path.extname(basename);
-            console.log('ext index:', index, 'next extension:', extension);
 
             //don't try to open the active file again
             if(extension === activeFileExtension) {
-                console.log('currentExt', currentExt, '===', activeFileExtension);
                 next();
             }
             else {
                 //find the next corresponding file
                 const fn = `${relativePath}/${activeFileBasename}${extension}`;
-                console.log('search for:', fn);
                 findFiles(fn).then(searchResults => {
                     if(searchResults && searchResults.length && searchResults.length > 0) {
-                        console.log('files found!', searchResults);
                         resolve(searchResults[0]);
                     }
                     else {
                         next();
                     }
                 }, error => {
-                    console.log('file not found', error);
                     reject('not able to find files');
                 });
             }
@@ -94,8 +85,7 @@ const findCorrespondingFile = ({ absoluteFilename, allExtensions, rootPath, find
 exports.findCorrespondingFile = findCorrespondingFile;
 
 function activate(context) {
-    //TODO: make this configurable
-    const allExtensions = ['.test.js', '.js', '.html'];
+    const allExtensions = vscode.workspace.getConfiguration('openCorrespondingFile').get('extensionList');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -104,7 +94,8 @@ function activate(context) {
         if(vscode.window.activeTextEditor && vscode.window.activeTextEditor.document) {
             const absoluteFilename = vscode.window.activeTextEditor.document.fileName;
             const rootPath = vscode.workspace.rootPath;
-
+            
+            //when activated, take the current file and find the next corresponding one
             findCorrespondingFile({ absoluteFilename, allExtensions, rootPath, findFiles }).then((file) => {
                 vscode.window.showTextDocument(file);
             }, (error) => {
